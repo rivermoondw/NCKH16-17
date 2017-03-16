b11fb2f5f7720df324c2bc5e3c9d90ecf0b913cd
rivermoondw <ztblueskytz@gmail.com>
2017-03-16 17:33:20 +0700

    master layout

---
+++ application/views/partials/footer.php
@@ -0,0 +1,3 @@
+<footer class="fc_footer">
+    <p>Nhóm thực hiện <span style="font-weight: bold">Power of Three</span></p>
+</footer>
---
+++ application/views/partials/header.php
@@ -0,0 +1,15 @@
+<header class="fc_header">
+    <div class="fc_innerwraper container">
+        <div class="rơw">
+            <div class="col-md-3 text-right">
+                <img src="<?php echo base_url();?>images/logo.png" />
+            </div>
+            <div class="col-md-6 search_query">
+                <input type="text">
+            </div>
+            <div class="col-md-3 text-right header_r">
+                <img src="<?php echo base_url();?>images/16904922_1681057578860437_6883583468305233941_o.jpg" height="40" width="40" class="img-circle avt_small"/>
+            </div>
+        </div>
+    </div> <!-- /.fc_innerwrapper -->
+</header> <!-- /header -->
